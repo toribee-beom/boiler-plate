75b3001e9514f28e6b447a343b17c7b991a77a65
toribee <alpinfun@naver.com>
2020-06-10 12:00:12 +0900

    secu setting

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-const mongoose = require('mongoose')
+const bodyParser = require('body-parser');
 
-mongoose.connect('mongodb+srv://toribee:rudqja1234@beomsclouster-k6hw1.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+//application/jsony
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose')
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
@@ -12,6 +22,20 @@ mongoose.connect('mongodb+srv://toribee:rudqja1234@beomsclouster-k6hw1.mongodb.n
 
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! 와 테스트임 그리고 다시 수정'))
+
+
+app.post('/register',(req,res) => {
+  //회원가입 정보 받아 DB 입력
+  
+  const user = new User(req.body)
+
+  user.save((err,userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
